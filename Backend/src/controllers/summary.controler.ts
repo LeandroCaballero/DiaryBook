05ea88e7409b95515607744153ea501b0d8adce6
LeandroCaballero <leandrorojo248@hotmail.com>
2023-07-24 23:03:40 -0300

    Add new summary modal

--- Backend/src/controllers/summary.controler.ts
+++ Backend/src/controllers/summary.controler.ts
@@ -4,38 +4,76 @@ import prisma from "../server/prisma"
 // export const getSummaries = () => {}
 
 export const createSummary = async (req: Request, res: Response) => {
-  const { dateStart, dateEnd, groupId, transactions } = req.body
+  const { dateStart, dateEnd, groupId, userId, transactions } = req.body
 
-  //   console.log(req.body)
+  console.log(req.body)
 
-  try {
-    const newSummary = await prisma.summary.create({
-      data: {
-        dateStart,
-        dateEnd,
-        Group: { connect: { id: groupId } },
-        Transactions: {
-          createMany: {
-            data: [
-              ...transactions.map(
-                (transaction: {
-                  buyerId: string
-                  debtorId: string
-                  amount: number
-                }) => ({
-                  buyerId: transaction.buyerId,
-                  debtorId: transaction.debtorId,
-                  amount: transaction.amount,
-                })
-              ),
-            ],
+  const findGroup = await prisma.group.findFirst({
+    where: {
+      id: groupId,
+    },
+    include: {
+      Purchases: {
+        where: {
+          dateBuy: {
+            gte: dateStart,
+            lte: dateEnd,
           },
         },
+        include: {
+          PurchaseItems: { include: { forUsers: true } },
+        },
       },
-    })
-    res.status(200).json(newSummary)
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: "Error intente más tarde" })
-  }
+    },
+  })
+
+  const initialValue: {
+    buyerId: string
+    debtorId: string
+    amount: number
+  }[] = []
+
+  const transactionsFinish = findGroup?.Purchases.reduce((acc, curr) => {
+    for (const purchaseItem of curr.PurchaseItems) {
+      // const find = purchaseItem.forUsers.some((user) =>
+      //   acc.find((el) => el.buyerId == user.id)
+      // )
+    }
+    // curr.PurchaseItems.map((purchaseItem) => {})
+    return [...acc]
+  }, initialValue)
+
+  console.log(findGroup)
+  res.status(200).json(findGroup)
+
+  // try {
+  //   const newSummary = await prisma.summary.create({
+  //     data: {
+  //       dateStart,
+  //       dateEnd,
+  //       Group: { connect: { id: groupId } },
+  //       Transactions: {
+  //         createMany: {
+  //           data: [
+  //             ...transactions.map(
+  //               (transaction: {
+  //                 buyerId: string
+  //                 debtorId: string
+  //                 amount: number
+  //               }) => ({
+  //                 buyerId: transaction.buyerId,
+  //                 debtorId: transaction.debtorId,
+  //                 amount: transaction.amount,
+  //               })
+  //             ),
+  //           ],
+  //         },
+  //       },
+  //     },
+  //   })
+  //   res.status(200).json(newSummary)
+  // } catch (error) {
+  //   console.log(error)
+  //   res.status(500).json({ message: "Error intente más tarde" })
+  // }
 }
